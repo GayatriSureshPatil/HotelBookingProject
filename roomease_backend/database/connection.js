2f6f2c3e4f7e1897d7bfd8f0c978c3c57d448c5b
Prajakta Patil <prajaktpatil2040206@gmail.com>
2026-05-01 18:37:10 +0530

    Update database configuration

--- roomease_backend/database/connection.js
+++ roomease_backend/database/connection.js
@@ -1,19 +1,24 @@
 const mysql2 = require("mysql2");
 require("dotenv").config();
 
-const db = mysql2.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "gayatri",
-  password: process.env.DB_PASSWORD || "23052006",
-  database: process.env.DB_NAME || "ramkrishna_my",
+const db = mysql2.createPool({
+  host: process.env.MYSQLHOST || "switchyard.proxy.rlwy.net",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "BPixaGdApCOEltZRiSjgUAQOFnEiOkaA",
+  database: process.env.MYSQLDATABASE || "ramkrishna_my",
+  port: process.env.MYSQLPORT || 28442,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.error("Error connecting to the database:", err.message);
+    console.error("Database connection failed:", err.message);
     return;
   }
-  console.log("Connected to the database");
+  console.log("Connected to the MySQL database");
+  connection.release();
 });
 
 module.exports = db;
